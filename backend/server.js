823f4c4a231aa08743e3e1dd4521c729c1924545
sumeetsrivastav07 <sumeetsrivastav0728@gmail.com>
2026-09-26 19:09:54 +0530

    feat: connect backend to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
+import dns from "dns";
 import "dotenv/config";
 import express from "express";
+import connectDB from "./config/db.js";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const app = express();
 
@@ -10,6 +14,9 @@ app.get("/api/health", (req, res) => {
         message: "Backend is running"
     });
 });
+
+connectDB();
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
